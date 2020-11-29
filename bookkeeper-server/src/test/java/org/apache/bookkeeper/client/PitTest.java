28edff91b4584605cab02d25e5051a1d0c1f1d7c
ezio <iixelite@gmail.com>
2020-11-29 17:58:12 +0100

    Pom modified in order to be able to skip tests if needed.

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/PitTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/PitTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 
 public class PitTest {
 
+    //LOCALMENTE
     //Make only one test, and buld only its module ! FINALLY !!
     //mvn -pl bookkeeper-server -am verify -Dtest=PitTest test -DfailIfNoTests=false
+
+    //ONLINE
+    //provare lo stesso comando, se non funge, provare il build in locale totale,
+    //specificando solo una classe di test, e lanciarla anche online
 
     @Test
     public void simpleTest(){
